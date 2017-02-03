d080d3cf541bb1eed853c35c0fa92b4b9eee9097
Masaki Osato <masakiosato@Masakis-MacBook-Pro.local>
2017-02-02 20:15:13 -0800

    Finalized the program

--- src/kek/Character.java
+++ src/kek/Character.java
@@ -5,15 +5,51 @@ import java.awt.Rectangle;
 public class Character {
 	Rectangle hitBox;
 	double accelX, accelY, velX, velY;
-	
-	Character(int x, int y, int width, int height) {
+	int windowWidth, windowHeight;
+	boolean jump = false;
+	Character(int x, int y, int width, int height, int ww, int wh) {
 		hitBox = new Rectangle(x, y, width, height);
+		windowWidth = ww;
+		windowHeight = wh;
 	}
-	
-	void jump(int wH) {
-		if (velY == 0) velY = 3;
-		accelY = -0.4;
-		hitBox.y -= velY;
-		velY += accelY;
+	void correct() {
+		if (hitBox.x < 0) {
+			hitBox.x = 0;
+			accelX = 0;
+			velX = 0;
+		}
+		if (hitBox.x > windowWidth - hitBox.width) {
+			hitBox.x = windowWidth - hitBox.width;
+			accelX = 0;
+			velX = 0;
+		}
+		if (hitBox.y > windowHeight - hitBox.height) {
+			hitBox.y = windowHeight - hitBox.height;
+			accelY = 0;
+			velY = 0;
+			jump = false;
+		}
+	}
+	void jump() {
+		if (jump) {
+			if (velY == 0) velY = 10;
+			accelY = -0.3;
+			hitBox.y -= (int)velY;
+			velY += accelY;
+		}
+	}
+	void move(boolean right, boolean left) {
+		if (right) {
+			accelX = -0.1;
+			velX = 3;
+		} else if (left) {
+			accelX = 0.1;
+			velX = -3;
+		}
+		hitBox.x += (int)velX;
+		if (velX <= 0.2 && velX >= -0.2) {
+			accelX = 0;
+			velX = 0;
+		} else velX += accelX;
 	}
 }
--- src/kek/kek.java
+++ src/kek/kek.java
@@ -11,15 +11,15 @@ import javax.swing.JPanel;
 
 public class kek extends JPanel implements KeyListener, Runnable {
 	static int windowWidth = 600, windowHeight = 400; 
-	Character player = new Character(220, windowHeight - 10, 10, 10);
-	boolean right = false, left = false, jump = false;
+	Character player = new Character(220, windowHeight - 10, 10, 10, windowWidth, windowHeight);
+	boolean right = false, left = false;
 	
 	public void keyTyped(KeyEvent e) {}
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		if (keyCode == KeyEvent.VK_RIGHT) right = true;
 		if (keyCode == KeyEvent.VK_LEFT) left = true;
-		if (keyCode == KeyEvent.VK_UP) jump = true;
+		if (keyCode == KeyEvent.VK_UP) player.jump = true;
 	}
 	public void keyReleased(KeyEvent e) {
 		int keyCode = e.getKeyCode();
@@ -28,42 +28,9 @@ public class kek extends JPanel implements KeyListener, Runnable {
 	}
 	public void run() {
 		while(true) {
-			if (right) {
-				player.accelX = -0.1;
-				player.velX = 3;
-			} else if (left) {
-				player.accelX = 0.1;
-				player.velX = -3;
-			}
-			player.hitBox.x += (int)player.velX;
-			if (player.velX <= 0.2 && player.velX >= -0.2) player.accelX = 0;
-			else player.velX += player.accelX;
-			
-			if (player.hitBox.x < 0) {
-				player.hitBox.x = 0;
-				player.accelX = 0;
-				player.velX = 0;
-			}
-			if (player.hitBox.x > windowWidth - player.hitBox.width) {
-				player.hitBox.x = windowWidth - player.hitBox.width;
-				player.accelX = 0;
-				player.velX = 0;
-			}
-			if (player.hitBox.y > windowHeight - player.hitBox.height) {
-				player.hitBox.y = windowHeight - player.hitBox.height;
-				player.accelY = 0;
-				player.velY = 0;
-			}
-			
-			if (jump) {
-				player.jump(windowHeight);
-				if (player.hitBox.y >= windowHeight - player.hitBox.height) {
-					jump = false;
-					player.accelY = 0;
-					player.velY = 0;
-				}
-			}
-			
+			player.move(right, left);
+			player.jump();
+			player.correct();
 			repaint();
 			try {Thread.sleep(10);} catch (Exception ex) {}
 		}
@@ -73,6 +40,11 @@ public class kek extends JPanel implements KeyListener, Runnable {
 		g.fillRect(0, 0, windowWidth, windowHeight);
 		g.setColor(Color.WHITE);
 		g.fillRect(player.hitBox.x, player.hitBox.y, player.hitBox.width, player.hitBox.height);
+		g.setColor(Color.RED);
+		g.drawString("accelX: " + player.accelX, 50, 25);
+		g.drawString("velX: " + player.velX, 50, 50);
+		g.drawString("accelY: " + player.accelY, 50, 75);
+		g.drawString("velY: " + player.velY, 50, 100);
 	}
 	public static void main(String[] args) {
 		kek game = new kek();
